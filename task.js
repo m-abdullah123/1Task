9d0adf5f5cfd32148fa4e8ed381f76a5f1da4aa3
m-abdullah123 <11muhamm22adabdullah@gmail.com>
2025-02-24 11:11:29 -0800

    1st

---
+++ task.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+let a = 15;
+let b = 23;
+console.log(a+b);
+if (a<15) {
+    console.log("A is less than 15");
+}
+else if(a=15){
+    console.log("A is Equal to 15");
+}
+else{
+    console.log("A is Greater than 15");
+}
